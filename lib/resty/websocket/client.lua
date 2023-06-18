6742c5c7fc6652f44677c61b27ef35a6e096ddd0
Nathan <chuck199411@gmail.com>
2023-06-18 22:24:08 +0800

    fix annotation

--- lib/resty/websocket/client.lua
+++ lib/resty/websocket/client.lua
@@ -2,7 +2,7 @@
 
 
 -- FIXME: this library is very rough and is currently just for testing
---        the websocket server.
+--        the websocket client.
 
 
 local wbproto = require "resty.websocket.protocol"
